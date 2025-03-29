44fea106a5e994a212e06209d54690efbe1d5163
Varad Pensalwar <varadpensalwar@gmail.com>
2025-03-29 13:17:16 +0530

    Update Contacts.js

--- src/components/Contacts.js
+++ src/components/Contacts.js
@@ -1,43 +1,65 @@
-import React, { useState, useRef } from 'react' 
+import React, { useState, useRef } from 'react'
 import CustomHook from './CustomHook';
 
 function Contacts() {
-const [listContacts] = useState([
-  {
-    title: 'Phone Number',
-    value: '+91 - 8669580734'
-  },{
-    title: 'Email',
-    value: 'varadpensalwar@gmail.com'
-  },{
-    title: 'linkedin',
-    value: 'https://www.linkedin.com/in/varad-pensalwar/'
-  }
-])
-const divs = useRef([]);
-const scrollTab = useRef();
-CustomHook(scrollTab, divs);
-
+  const [listContacts] = useState([
+    {
+      title: 'Phone Number',
+      value: '+91 - 8669580734'
+    },
+    {
+      title: 'Email',
+      value: 'varadpensalwar@gmail.com',
+      isEmail: true
+    },
+    {
+      title: 'github',
+      value: 'https://github.com/Varadpensalwar',
+      isGithub: true
+    }
+  ])
+  const divs = useRef([]);
+  const scrollTab = useRef();
+  CustomHook(scrollTab, divs);
+  
   return (
     <section className='contacts' ref={scrollTab}>
-       <div className="title" ref={(el) => el && divs.current.push(el)}>
+      <div className="title" ref={(el) => el && divs.current.push(el)}>
         This is my Contacts
-       </div>
-       <div className="des" ref={(el) => el && divs.current.push(el)}>
+      </div>
+      <div className="des" ref={(el) => el && divs.current.push(el)}>
         {/* 20 */}
         Lorem ipsum dolor sit amet consectetur adipisicing elit. Ullam perspiciatis quae veniam amet nesciunt voluptatibus quis consectetur consequatur quisquam harum.
-       </div>
-       <div className="list"  ref={(el) => el && divs.current.push(el)}>
+      </div>
+      <div className="list" ref={(el) => el && divs.current.push(el)}>
         {
           listContacts.map((value, key) => (
             <div className='item' key={key}>
               <h3>{value.title}</h3>
-              <div>{value.value}</div>
+              <div>
+                {value.isEmail ? (
+                  <a href={`mailto:${value.value}`} title={value.value}>
+                    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round">
+                      <rect x="2" y="4" width="20" height="16" rx="2" ry="2"></rect>
+                      <path d="M22 7l-10 5-10-5"></path>
+                    </svg>
+                  </a>
+                ) : value.isGithub ? (
+                  <a href={value.value} target="_blank" rel="noopener noreferrer" title={value.value}>
+                    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round">
+                      <path d="M9 19c-5 1.5-5-2.5-7-3m14 6v-3.87a3.37 3.37 0 0 0-.94-2.61c3.14-.35 6.44-1.54 6.44-7A5.44 5.44 0 0 0 20 4.77 5.07 5.07 0 0 0 19.91 1S18.73.65 16 2.48a13.38 13.38 0 0 0-7 0C6.27.65 5.09 1 5.09 1A5.07 5.07 0 0 0 5 4.77a5.44 5.44 0 0 0-1.5 3.78c0 5.42 3.3 6.61 6.44 7A3.37 3.37 0 0 0 9 18.13V22"></path>
+                    </svg>
+                  </a>
+                ) : (
+                  value.value
+                )}
+              </div>
             </div>
           ))
         }
-       </div>
+      </div>
     </section>
   )
 }
+
 export default Contacts
