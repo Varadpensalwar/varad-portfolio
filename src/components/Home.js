17495b79b28e2100f1cff21bf90d918663fe445b
Varad Pensalwar <varadpensalwar@gmail.com>
2025-03-29 11:52:31 +0530

    Initial commit

---
+++ src/components/Home.js
@@ -0,0 +1,37 @@
+import React, { useRef } from 'react' 
+import CustomHook from './CustomHook';
+function Home() {
+const scrollTab = useRef();
+CustomHook(scrollTab);
+
+return (
+    <section ref={scrollTab} className='home'>
+        <div className="content">
+            <div className="name">
+                MY NAME IS <span>Varad</span>
+            </div>
+            <div className="des">
+                {/* 30 */}
+                I am Varad, an aspiring Programmer pursuing his B.Tech Major in Artificial Intelligence and Machine from the Sanjay Ghodawat University, Kolhapur. On top of that I am Web Development Enthusiast, who loves to code.
+            </div>
+            
+            <a href="/democv.pdf" target="_blank" rel="noopener noreferrer" className='animation active '>
+                Download My CV
+            </a>
+        </div>
+        <div className="avatar">
+            <div className="card">
+                <img src="/avatar.jpg" alt="" />
+                <div className="info">
+                    <div>Developer</div>
+                    <div>VietNamese</div>
+                    <div>03/12</div>
+                    <div>Male</div>
+                </div>
+            </div>
+        </div>
+    </section>
+    )
+}
+
+export default Home
